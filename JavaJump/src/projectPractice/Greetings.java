8a741267ab86e814fae19e0556ff57fbc687c903
Sayyed Waliullah <jiesm2020@gmail.com>
2022-03-21 19:41:04 +0530

    first Commit..

---
+++ JavaJump/src/projectPractice/Greetings.java
@@ -0,0 +1,10 @@
+package projectPractice;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Assalamu Alaikum.");
+	}
+
+}
