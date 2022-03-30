20daf9c9bba3035a38eb50b4d40a788c848cc11b
Sayyed Waliullah <jiesm2020@gmail.com>
2022-03-30 05:51:10 +0530

    First Commit..

---
+++ JavaJump/src/day1/InputS.java
@@ -0,0 +1,15 @@
+package day1;
+import java.util.*;
+public class InputS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter Your Name..");
+		Scanner sc=new Scanner(System.in);
+		
+String name=sc.next();
+System.out.println("Sayyed");
+System.out.println("Hello! "+name);
+	}
+
+}
